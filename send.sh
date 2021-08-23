efaa5e7b32a477c924a8733a0fa3c7e071b92e3c
shang <1415317271@qq.com>
2021-08-23 11:45:31 +0800

    modify subject

---
+++ send.sh
@@ -0,0 +1,1 @@
+git send-email -cc cuibixuan@huawei.com -to kernel@openeuler.org -cc 1415317271@qq.com --suppress-cc=all *
---
+++ test.sh
@@ -0,0 +1,1 @@
+git send-email -to cyber7an@tom.comg -cc 1415317271@qq.com --suppress-cc=all 00*
